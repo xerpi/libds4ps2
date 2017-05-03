d7aaa5c5c5b857e4adaa4791e25d3654a4252d71
Sergi Granell <xerpi.g.12@gmail.com>
2017-05-03 18:39:51 +0200

    Update to latest SDK

---
+++ iop/irx_imports.h
@@ -0,0 +1,14 @@
+#ifndef IOP_IRX_IMPORTS_H
+#define IOP_IRX_IMPORTS_H
+
+#include "irx.h"
+
+#include "loadcore.h"
+#include "sifcmd.h"
+#include "sysclib.h"
+#include "sysmem.h"
+#include "thbase.h"
+#include "thsemap.h"
+#include "usbd.h"
+
+#endif
